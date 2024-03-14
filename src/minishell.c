a726886731898b28da4778161923cf4544abf347
Juan Bautista Delorme Pinedo <jdelorme@c2r1s1.42madrid.com>
2024-03-14 18:04:24 +0100

    restruct

--- src/minishell.c
+++ src/minishell.c
@@ -1,55 +1,36 @@
-/* ***********************************IMPORTANTE*************************************** */
-// * Manual de BASH *  https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html */
-// * Compilar proyecto * gcc libft/libft.a minishell.c -lreadline -ltermcap             */
-// * Cerrar el programa * Ctrl + D                                                      */
-// * Instalar better comments en VS para ver los comentarios mas claros                 */
-/* ************************************************************************************ */
-
 #include "../inc/minishell.h"
-
-/* ************************************************************************** */
-/*  Funcion que comprueba la existencia de builtins en el input del usuario   */
-/*  y llama a ejecutar el builtin indicado                                    */
-// ! Falta expandir las variables antes de llamar a comparar cualquiera       */
-/* ************************************************************************** */
 
 void    ft_builtins(t_msh *commands)
 {
     if (ft_strcmp(commands->input, "echo") == 0)
     {
-        printf("\033[31mEjecutado echo...\033[0m\n");
+        printf("\033[34mEjecutado echo...\033[0m\n");
     }
     if (ft_strcmp(commands->input, "cd") == 0)
     {
-        printf("\033[31mEjecutado cd...\033[0m\n");
+        printf("\033[34mEjecutado cd...\033[0m\n");
     }
     if (ft_strcmp(commands->input, "pwd") == 0)
     {
-        printf("\033[31mEjecutado pwd...\033[0m\n");
+        printf("\033[34mEjecutado pwd...\033[0m\n");
     }
     if (ft_strcmp(commands->input, "export") == 0)
     {
-        printf("\033[31mEjecutado export...\033[0m\n");
+        printf("\033[34mEjecutado export...\033[0m\n");
     }
     if (ft_strcmp(commands->input, "unset") == 0)
     {
-        printf("\033[31mEjecutado unset...\033[0m\n");
+        printf("\033[34mEjecutado unset...\033[0m\n");
     }
     if (ft_strcmp(commands->input, "env") == 0)
     {
-        printf("\033[31mEjecutado env...\033[0m\n");
+        printf("\033[34mEjecutado env...\033[0m\n");
     }
     if (ft_strcmp(commands->input, "exit") == 0)
     {
-        printf("\033[31mEjecutado exit...\033[0m\n");
+        printf("\033[34mEjecutado exit...\033[0m\n");
     }
 }
-
-/* *********************************************************************************** */
-/*  Funcion que comprueba que en el input las comillas simples y dobles introducidas   */
-/*cerradas en caso contrario no es necesario gestionarlo por ende se cierra el programa*/
-/* *********************************************************************************** */
-
 int ft_incomplete_quotes(t_msh *commands)
 {
     int i;
@@ -59,94 +40,76 @@ int ft_incomplete_quotes(t_msh *commands)
     quote = 0;
     while (commands->input[++i] != '\0')
     {
-        if (!quote && (commands->input[i] == '\'' || commands->input[i] == '\"') 
-			&& (commands->input[i - 1] != '\\' || i == 0))
+        if (!quote && (commands->input[i] == SQUOTES || commands->input[i] == DQUOTES) 
+			&& (commands->input[i - 1] != BACKSLASH || i == 0))
 			    quote = commands->input[i];
 		else if (commands->input[i] == quote && commands->input[i - 1] != '\\')
 			quote = '\0';
     } 
     return (quote != '\0');
 }
-/* ********************************************************************************** */
-/*  1. Llama a comprobar si las comillas simples estan cerradas                       */
-/*  2. LLama a comporbar si las comillas dobles estan cerradas                        */  
-/*  3. Llama a comprobar si hay algun built-in y lo ejecuta                           */ 
-/*                                                                                    *  
-    ! Falta gestionar que el programa no se cierre si las comillas no estas cerradas, 
-    ! no gestionar que se cierren estas, sino simplemente esperar nuevo input.
-    ! Si variables globales contienen comillas, salta error cuando no debería. 
-*/
-/* ********************************************************************************** */
-
 void    ft_manage(t_msh *commands)
 {
     if (ft_incomplete_quotes(commands) == 1)
     {
-        printf("--->Syntax error, quotes not closed\n");
+        printf("\033[34mSyntax error, quotes not closed\033[0m\n");
         return ;
     }
     ft_expand_var(commands);
     ft_builtins(commands);
 
 }
-/* ********************************************************************************* */
-/*  1. TGETSTR = intenta borrar ^C del terminal mediante "cl" (clear) (No funcoiona) */
-/*  2. Si es SIGINT, imprime un salto de línea, para limpiat a terminal.             *
-    ! Falta borrar ^C que se imprime en terminal al presionar las teclas...          */
-/* ********************************************************************************* */
-
 void    ft_sigint(int sign)
 {
     char *get_str;
-	printf("---->%i", sign);
-    get_str = tgetstr("cl", NULL);
     if (sign == SIGINT)
     {
         tputs(get_str, 1, putchar);
         ft_putchar_fd('\n', 1);
     }
-    // 
 }
-/* ******************************************************************************************************************************************************* */
-/*  1. SIGINT = verifica si el usuario presiona ctrl+c;                                                                                                    */
-/*  2. SIGQUIT = verifica si el usuario presiona ctrl+d;                                                                                                   */
-/*  3. READLINE = Dentro de un bucle infinito, espera la entrada del usuario, y la guarda en input, asignando memoria automaticamente                      */
-/*  4. ENVP = Se llama a copiar en enviroment de la shell, para despues modificar su nivel si se ejecuta una shell en una shell                            */
-/*  5. FT_MANAGE = Llama a gestionar cierta parte del parseo del input                                                                                     */
-/*  6. WRITE_HISTORY = Genera una arhivo de los comandos que han sido ejecutados y añadidos mediante add_history                                           *
-    ! SYSTEM = Es una cochinada para que se ejecuten los comandos mientras se realiza el parseo y no la parte ejecutora del programa no esta implementada. */
-/* ******************************************************************************************************************************************************* */
-
-int main(int argc, char **argv, char **envp) 
+void    ft_init_struct(t_msh *commands)
 {
-    t_msh commands;
-
-    if (argc != 1)
-        return(1);
-    
-    commands.envp = ft_copy_envp(envp);
-    commands.envp = ft_manage_shlvl(commands.envp);
+    commands->cmd = NULL;
+    commands->envp = NULL;
+    commands->evar = NULL;
+    commands->input = NULL;
+}
+void    ft_shlvl(t_msh *commands, char **envp)
+{
+    commands->envp = ft_copy_envp(envp);
+    commands->envp = ft_manage_shlvl(commands->envp);
+}
+void    ft_handle_readline(t_msh *commands)
+{
     while (1)
     {
         signal(SIGINT, ft_sigint);
 		signal(SIGQUIT, ft_sigint);
-        commands.input = readline("🐚Minishell-42$");
-        if (commands.input == NULL)
+        commands->input = readline("🐚Minishell-42$");
+        if (commands->input == NULL)
         {
            printf("\nNo se ingresó ninguna entrada.\n");
            break;
         }
         else
         {
-            // for (int i = 0; commands.envp[i]; i++)
-            //     printf("\033[31m%s\033[0m\n", commands.envp[i]);
-            add_history(commands.input);
-            ft_manage(&commands);
-            printf("Variable expandida: %s\n", commands.evar);
-            system(commands.input);
-            free(commands.input);
+            add_history(commands->input);
+            ft_manage(commands);
+            system(commands->input);
+            free(commands->input);
         }
     }
+}
+int main(int argc, char **argv, char **envp) 
+{
+    t_msh commands;
+
+    if (argc != 1 || !argv)
+        return(1);
+    ft_init_struct(&commands);
+    ft_shlvl(&commands, envp);
+    ft_handle_readline(&commands);
     write_history("Historial");
     return 0;
 }
