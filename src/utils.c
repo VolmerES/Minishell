a8e7acaf3136ccea4be10221c135d016a6560439
Juan Bautista Delorme Pinedo <jdelorme@c2r11s5.42madrid.com>
2024-03-15 18:34:53 +0100

    add protection and null set

--- src/utils.c
+++ src/utils.c
@@ -9,6 +9,8 @@ int	ft_strcmp(const char *str1, const char *str2)
 {
 	size_t	i;
 
+	if (!str1 || !str2)
+		return (0);
 	i = 0;
 	while ((str1[i] || str2[i]))
 	{
@@ -23,7 +25,13 @@ void    ft_free_matrix(char **env)
     int i;
 
     i = -1;
+	if (!env)
+		return ;
     while (env[++i])
+	{
         free(env[i]);
+		*env[i] = NULL;
+	}
+	*env = NULL;
     free(env);
 }
