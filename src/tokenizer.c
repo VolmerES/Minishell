2192603d6213e4cf1b2c39262bfe0b746a6b05ee
Juan Bautista Delorme Pinedo <jdelorme@c2r7s4.42madrid.com>
2024-04-11 14:57:22 +0200

    Command & args add to struct correctly

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,167 +1,197 @@
 #include "../inc/minishell.h"
 
-static void ft_is_commands_and_args(t_msh *commands, int *i)
+static void ft_arguments(t_msh *commands, int *i, int *j)
 {
-    
+    while (commands->cmds[*i]->cmd[*j] == SPACE)
+    {
+        (*j)++;
+    }
+    while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != SPACE && commands->cmds[*i]->cmd[*j] != '<' && commands->cmds[*i]->cmd[*j] != '>')
+    {
+        if (commands->cmds[*i]->cmd[*j] == SPACE)
+        {
+            (*j)++;
+        }
+        else
+        {
+            int start;
+            int k;
+
+            k = 0;
+            start = *j;
+            while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != SPACE && commands->cmds[*i]->cmd[*j] != '<' && commands->cmds[*i]->cmd[*j] != '>')
+            {
+                (*j)++;
+            }
+            commands->cmds[*i]->args = malloc(sizeof(t_cmd()));
+            commands->cmds[*i]->args[k] = ft_substr(commands->cmds[*i]->cmd, start, *j - start);
+            printf("Argumento: %s\n", commands->cmds[*i]->args[k]);
+            k++;
+        }
+    }
+    commands->cmds[*i]->args = NULL;
+}
+static void ft_is_command(t_msh *commands, int *i, int *j)
+{
+    int start;
+
+    start = *j;
+    while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != ' ' && commands->cmds[*i]->cmd[*j] != '<' && commands->cmds[*i]->cmd[*j] != '>')
+    {
+        (*j)++;
+    }
+    commands->cmds[*i]->cmd_main = ft_substr(commands->cmds[*i]->cmd, start, *j - start);
+    printf("Comando main: %s\n", commands->cmds[*i]->cmd_main);
+    while (commands->cmds[*i]->cmd[*j] != '<' && commands->cmds[*i]->cmd[*j] != '>' && commands->cmds[*i]->cmd[*j] != '\0')
+    {
+        ft_arguments(commands, i, j);
+        *j += 1;
+    }
 }
 
-static void   ft_is_outfile_trunc(t_msh *commands, int *i)
+static void   ft_is_outfile_trunc(t_msh *commands, int *i, int *j)
 {
     t_file  *outfile;
-    int     j;
     char    *filename;
     int     start;
 
     outfile = malloc(sizeof(t_file));
     filename = "";
-    j = 0;
-    while (commands->cmds[*i]->cmd[j] != '>' && commands->cmds[*i]->cmd[j] != '\0')
-        j++;
-    if (commands->cmds[*i]->cmd[j] == '>')
-        j++;
-    while (commands->cmds[*i]->cmd[j] == ' ')
-        j++;
-    start = j;
-    while (commands->cmds[*i]->cmd[j] != '\0' && commands->cmds[*i]->cmd[j] != ' ')
-    {
-        j++;
-    }
-    filename = ft_substr(commands->cmds[*i]->cmd, start, j - start);
+    while (commands->cmds[*i]->cmd[*j] != '>' && commands->cmds[*i]->cmd[*j] != '\0')
+        (*j)++;
+    if (commands->cmds[*i]->cmd[*j] == '>')
+        (*j)++;
+    while (commands->cmds[*i]->cmd[*j] == ' ')
+        (*j)++;
+    start = *j;
+    while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != ' ')
+        (*j)++;
+    filename = ft_substr(commands->cmds[*i]->cmd, start, *j - start);
     outfile->filename = filename;
     outfile->type = OUTFILE_TRUNC;
     commands->cmds[*i]->outfile = outfile;
     printf("Trunc: %s\n", outfile->filename);
 }
 
-static void    ft_is_outfile_append(t_msh *commands, int *i)
+static void    ft_is_outfile_append(t_msh *commands, int *i, int *j)
 {
     t_file  *outfile;
-    int     j;
     char    *filename;
     int     start;
 
     outfile = malloc(sizeof(t_file));
     filename = "";
-    j = 0;
-    while (commands->cmds[*i]->cmd[j] != '\0') 
+    while (commands->cmds[*i]->cmd[*j] != '\0') 
     {
-        if (commands->cmds[*i]->cmd[j] == '>' && commands->cmds[*i]->cmd[j + 1] == '>')
+        if (commands->cmds[*i]->cmd[*j] == '>' && commands->cmds[*i]->cmd[*j + 1] == '>')
             break;
-        j++;
+        (*j)++;
     }
-    if (commands->cmds[*i]->cmd[j] == '>' && commands->cmds[*i]->cmd[j + 1] == '>')
-        j += 2;
-    while (commands->cmds[*i]->cmd[j] == ' ')
-        j++;
-    start = j;
-    while (commands->cmds[*i]->cmd[j] != '\0' && commands->cmds[*i]->cmd[j] != ' ' 
-    && commands->cmds[*i]->cmd[j] != '>')
+    if (commands->cmds[*i]->cmd[*j] == '>' && commands->cmds[*i]->cmd[*j + 1] == '>')
+        (*j) += 2;
+    while (commands->cmds[*i]->cmd[*j] == ' ')
+        (*j)++;
+    start = *j;
+    while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != ' ' 
+    && commands->cmds[*i]->cmd[*j] != '>')
     {
-        j++;
+        (*j)++;
     }
-    filename = ft_substr(commands->cmds[*i]->cmd, start, j - start);
+    filename = ft_substr(commands->cmds[*i]->cmd, start, *j - start);
     outfile->filename = filename;
     outfile->type = OUTFILE_APPEND;
     commands->cmds[*i]->outfile = outfile;
     printf("Append: %s\n", outfile->filename);
 }
 
-static void    ft_is_infile(t_msh *commands, int *i)
+static void    ft_is_infile(t_msh *commands, int *i, int  *j)
 {
     t_file  *infile;
-    int     j;
     char    *filename;
     int     start;
 
     infile = malloc(sizeof(t_file));
     filename = "";
-    j = 0;
-    while (commands->cmds[*i]->cmd[j] != '<' && commands->cmds[*i]->cmd[j] != '\0')
-        j++;
-    if (commands->cmds[*i]->cmd[j] == '<')
-        j++;
-    while (commands->cmds[*i]->cmd[j] == ' ')
-        j++;
-    start = j;
-    while (commands->cmds[*i]->cmd[j] != '\0' && commands->cmds[*i]->cmd[j] != ' ' 
-    && commands->cmds[*i]->cmd[j] != '<')
+    while (commands->cmds[*i]->cmd[*j] != '<' && commands->cmds[*i]->cmd[*j] != '\0')
+        (*j)++;
+    if (commands->cmds[*i]->cmd[*j] == '<')
+        (*j)++;
+    while (commands->cmds[*i]->cmd[*j] == ' ')
+        (*j)++;
+    start = *j;
+    while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != ' ' 
+    && commands->cmds[*i]->cmd[*j] != '<')
     {
-        j++;
+        (*j)++;
     }
-    filename = ft_substr(commands->cmds[*i]->cmd, start, j - start);
+    filename = ft_substr(commands->cmds[*i]->cmd, start, *j - start);
     infile->filename = filename;
     infile->type = INFILE_NORMAL;
     commands->cmds[*i]->infile = infile;
     printf("Infile: %s\n", infile->filename);
 }
 
-static void    ft_is_infile_here_doc(t_msh *commands, int *i)
+static void    ft_is_infile_here_doc(t_msh *commands, int *i, int *j)
 {
     t_file  *infile;
-    int     j;
     char    *filename;
     int     start;
 
     infile = malloc(sizeof(t_file));
     filename = "";
-    j = 0;
-    while (commands->cmds[*i]->cmd[j] != '\0') 
+    while (commands->cmds[*i]->cmd[*j] != '\0') 
     {
-        if (commands->cmds[*i]->cmd[j] == '<' && commands->cmds[*i]->cmd[j + 1] == '<')
+        if (commands->cmds[*i]->cmd[*j] == '<' && commands->cmds[*i]->cmd[*j + 1] == '<')
             break;
-        j++;
+        (*j)++;
     }
-    if (commands->cmds[*i]->cmd[j] == '<' && commands->cmds[*i]->cmd[j + 1] == '<')
-        j += 2;
-    while (commands->cmds[*i]->cmd[j] == ' ')
-        j++;
-    start = j;
-    while (commands->cmds[*i]->cmd[j] != '\0' && commands->cmds[*i]->cmd[j] != ' ' 
-    && commands->cmds[*i]->cmd[j] != '<')
+    if (commands->cmds[*i]->cmd[*j] == '<' && commands->cmds[*i]->cmd[*j + 1] == '<')
+        (*j) += 2;
+    while (commands->cmds[*i]->cmd[*j] == ' ')
+        (*j)++;
+    start = *j;
+    while (commands->cmds[*i]->cmd[*j] != '\0' && commands->cmds[*i]->cmd[*j] != ' ' 
+    && commands->cmds[*i]->cmd[*j] != '<')
     {
-        j++;
+        (*j)++;
     }
-    filename = ft_substr(commands->cmds[*i]->cmd, start, j - start);
+    filename = ft_substr(commands->cmds[*i]->cmd, start, *j - start);
     infile->filename = filename;
     infile->type = INFILE_HERE_DOC;
     commands->cmds[*i]->infile = infile;
     printf("Here_doc: %s\n", infile->filename);
 }
 
-static void    ft_tokenize_command(t_msh *commands, int index)
+static void    ft_tokenize_command(t_msh *commands, int *i)
 {
-    int *i;
-    int skip;
     int j;
 
     j = 0;
-    *i = index;
     while(commands->cmds[*i]->cmd[j] != '\0')
     {
         if (commands->cmds[*i]->cmd[j] == '<')
         {
             if (commands->cmds[*i]->cmd[j + 1] == '<')
             {
-                ft_is_infile_here_doc(commands, i); // <<: here_doc
-                j++; // Skip the next '<'
+                ft_is_infile_here_doc(commands, i, &j); // <<: here_doc
             }
             else 
-                ft_is_infile(commands, i); // <: Infile normal
+                ft_is_infile(commands, i, &j); // <: Infile normal
         }
         else if (commands->cmds[*i]->cmd[j] == '>')
         {
             if (commands->cmds[*i]->cmd[j + 1] == '>')
             {
-                ft_is_outfile_append(commands, i); // >>: Append
+                ft_is_outfile_append(commands, i, &j); // >>: Append
                 j++; // Skip the next '>'
             }
             else
-                ft_is_outfile_trunc(commands, i); // >: TRUNC
+                ft_is_outfile_trunc(commands, i, &j); // >: TRUNC
         }
         //condicion para los segmentar los comandos y argumentos
         else
         {
-            ft_is_commands_and_args(commands, i);
+            ft_is_command(commands, i, &j);
         }
         j++;
     }
@@ -173,7 +203,7 @@ void		ft_tokenize(t_msh *commands)
     i = 0;
     while (commands->cmds[i] != NULL)
     {
-        ft_tokenize_command(commands, i);
+        ft_tokenize_command(commands, &i);
         i++;
     }
 }
