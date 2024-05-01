345fb24c10e8345d65664d3fb498f5073c106193
LUIS DAVID <luis.david.10@hotmail.com>
2024-05-01 17:27:27 +0200

    fix: next

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -3,17 +3,8 @@
 void	ft_builtins(t_msh *commands)
 {
 	int i;
-	// char *ptr[] = {ft_strdup("DAVID=123"), ft_strdup("DAVID=1234"), ft_strdup("lpastor=old"), ft_strdup("lpastor=new"), ft_strdup("DAVID=ldiaz-ra"), NULL};
 
 	i = 0;
-
-	
-	// commands->cmds[0]->args = ptr;
-	// cd_builtin(commands, i);
-	// pwd_builtin();
-	// export_builtin(commands, i);
-	// export_builtin(commands, i);
-
 	while (commands->cmds[i])
 	{
 		if (ft_strcmp(commands->cmds[i]->cmd_main, "cd") == 0)
@@ -32,26 +23,4 @@ void	ft_builtins(t_msh *commands)
 			executor_manage(commands, i);
 		i++;
 	}
-
-
-    // if (ft_strcmp(commands->input, "echo") == 0)
-    // {
-    //     printf("\033[34mEjecutado echo...\033[0m\n");
-    // }
-    // if (ft_strcmp(commands->input, "export") == 0)
-    // {
-    //     printf("\033[34mEjecutado export...\033[0m\n");
-    // }
-    // if (ft_strcmp(commands->input, "unset") == 0)
-    // {
-    //     printf("\033[34mEjecutado unset...\033[0m\n");
-    // }
-    // if (ft_strcmp(commands->input, "env") == 0)
-    // {
-    //     printf("\033[34mEjecutado env...\033[0m\n");
-    // }
-    // if (ft_strcmp(commands->input, "exit") == 0)
-    // {
-    //     printf("\033[34mEjecutado exit...\033[0m\n");
-    // }
 }
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 13:42:29 by ldiaz-ra          #+#    #+#             */
-/*   Updated: 2024/05/01 16:15:16 by ldiaz-ra         ###   ########.fr       */
+/*   Updated: 2024/05/01 17:09:06 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void    executor_manage(t_msh *commands, int num_command)
 {
    if (commands->cmds[num_command]->infile != NULL)
    {
-    
+    printf("hoa");
    }
 }
