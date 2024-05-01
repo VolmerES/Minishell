50b4859387c3bd597eb56816928795a26ea56095
LUIS DAVID <luis.david.10@hotmail.com>
2024-05-01 15:36:11 +0200

    fix: temporal if

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 11:28:40 by david             #+#    #+#             */
-/*   Updated: 2024/04/13 12:07:27 by david            ###   ########.fr       */
+/*   Updated: 2024/05/01 13:23:59 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int	export_builtin(t_msh *commands, int i)
 {
-	char **var;
-
-	if (commands->cmds[i]->args == NULL)
+	// if (commands->cmds[i]->args == NULL)
+	if (commands->cmds[i]->args[0] == NULL)
 		export_empty(commands);
 	else
 		manage_export(commands, i);
