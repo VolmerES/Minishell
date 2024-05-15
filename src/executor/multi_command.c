233d2f683cb40d37ab6ba88b2c4bb06e2337d573
David Diaz <luis.david.10@hotmail.com>
2024-05-15 13:22:22 +0200

    update

--- src/executor/multi_command.c
+++ src/executor/multi_command.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   executor.c                                         :+:      :+:    :+:   */
+/*   multi_command.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/01 13:42:29 by ldiaz-ra          #+#    #+#             */
-/*   Updated: 2024/05/12 16:05:34 by david            ###   ########.fr       */
+/*   Created: 2024/05/12 14:17:34 by david             #+#    #+#             */
+/*   Updated: 2024/05/12 16:29:45 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void    executor_manage(t_msh *commands)
+// void	process_one(char **argv, char **env, t_pipex *pipex)
+void	multi_command(t_msh *commands)
 {
-	int i;
-
-	i = 0;
-    if (commands->parser.cmd_index == 1)
-    {
-        if (is_builtins(commands, i))
-            bd_one_command(commands);
-        else
-            one_command(commands);
-    }
-    else
-    {
-		// if (is_builtins(commands, i))
-		// 	//bd_multi_command(commands);
-		// else
-			multi_command(commands);
-    }
+	
+	
 }
