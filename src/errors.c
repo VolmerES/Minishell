9d3084c13b6544a29e121dd49ed9868f2bd36af6
LUIS DAVID <luis.david.10@hotmail.com>
2024-06-01 16:55:07 +0200

    fix: create method err

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 15:26:28 by ldiaz-ra          #+#    #+#             */
-/*   Updated: 2024/05/25 14:40:37 by ldiaz-ra         ###   ########.fr       */
+/*   Updated: 2024/06/01 16:34:52 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,11 @@ void	exit_(t_err_msg error)
 	else if (error == 2)
 		perror("MALLOC");
 	exit(1);
+}
+
+void	exit_err(t_msh *commands, int cmd_i)
+{
+	ft_putstr_fd(commands->cmds[cmd_i]->cmd_main, STDERR_FILENO);
+	ft_putstr_fd(": command not found\n", STDERR_FILENO);
+	exit(127);
 }
