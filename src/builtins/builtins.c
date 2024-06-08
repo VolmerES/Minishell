51dec6fc9aa80d4eefeb6c67c9974ec7f5af504e
LUIS DAVID <luis.david.10@hotmail.com>
2024-06-08 17:45:00 +0200

    fix: last out

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -7,7 +7,7 @@ void	ft_builtins(t_msh *commands, int i)
 		if (ft_strcmp(commands->cmds[i]->cmd_main, "cd") == 0)
 			cd_builtin(commands, i);
 		else if (ft_strcmp(commands->cmds[i]->cmd_main, "pwd") == 0)
-			pwd_builtin();
+			commands->last_out = pwd_builtin();
 		else if (ft_strcmp(commands->cmds[i]->cmd_main, "export") == 0)
 			export_builtin(commands, i);
 		else if (ft_strcmp(commands->cmds[i]->cmd_main, "echo") == 0)
