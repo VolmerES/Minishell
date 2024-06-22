13f28f9e5c51b4c5eaa5eb52b916149dc9c2b927
LUIS DAVID <luis.david.10@hotmail.com>
2024-06-22 20:33:10 +0200

    fix:  exit builtin ok

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,11 +6,27 @@
 /*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 17:25:02 by david             #+#    #+#             */
-/*   Updated: 2024/06/08 19:04:26 by ldiaz-ra         ###   ########.fr       */
+/*   Updated: 2024/06/22 20:30:17 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static void check_arg(char *arg)
+{
+	int i;
+	
+	i = 0;
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+		{
+			printf("Minishell: %s: numeric argument required\n", arg);
+			exit(2);
+		}
+		i++;
+	}
+}
 
 void	exit_builtin(t_msh *commands, int cmd_i)
 {
@@ -18,17 +34,10 @@ void	exit_builtin(t_msh *commands, int cmd_i)
 	if (check_num_args(commands, cmd_i) > 1)
 	{
 		commands->last_out = 1;
-		if(!ft_isdigit(ft_atoi(commands->cmds[cmd_i]->args[0])))
-		{
-			printf("exit: %s: numeric argument required\n", commands->cmds[cmd_i]->args[0]);
-			exit(2);
-		}
+		check_arg(commands->cmds[cmd_i]->args[0]);
 		printf("exit: too many arguments\n");
 		return ;
 	}
-	else
-	{
-
-	}
-	exit(commands->last_out);
+	check_arg(commands->cmds[cmd_i]->args[0]);
+	exit(ft_atoi(commands->cmds[cmd_i]->args[0]) % 256);
 }
