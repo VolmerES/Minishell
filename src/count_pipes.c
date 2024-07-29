d92034bef1489a65a0ec7b475ea4b3cb8a06fc67
LUIS DAVID <luis.david.10@hotmail.com>
2024-07-29 20:01:02 +0200

    fix: contar la cantidad de comandos sin tener en cuentra los pipes dentro de comillas

--- src/count_pipes.c
+++ src/count_pipes.c
@@ -2,16 +2,24 @@
 
 int ft_count_pipes(t_msh *commands)
 {
-	int i;
-	int count;
+    int i;
+    int count;
+    int in_single_quote;
+    int in_double_quote;
 
-	i = 0;
-	count = 0;
-	while (commands->input[i])
-	{
-		if(commands->input[i] == '|')
-			count++;
-		i++;
-	}
-	return (count + 1);
+    i = 0;
+    count = 0;
+    in_single_quote = 0;
+    in_double_quote = 0;
+    while (commands->input[i])
+    {
+        if (commands->input[i] == SQUOTES && !in_double_quote)
+            in_single_quote = !in_single_quote;
+        else if (commands->input[i] == DQUOTES && !in_single_quote)
+            in_double_quote = !in_double_quote;
+        if (commands->input[i] == PIPE && !in_single_quote && !in_double_quote)
+            count++;
+        i++;
+    }
+    return (count + 1);
 }
