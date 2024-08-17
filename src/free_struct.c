97f0cf6f39cda70499ddbe8680ef78266b9c75e6
LUIS DAVID <luis.david.10@hotmail.com>
2024-08-17 13:26:36 +0200

    fix: freeing pointers within the file structure

--- src/free_struct.c
+++ src/free_struct.c
@@ -1,7 +1,22 @@
-	
-	
 	#include "../inc/minishell.h"
 
+	static void free_aux(t_file **list)
+	{
+		int i;
+
+		i = 0;
+		while (list[i])
+		{
+			if (list[i]->filename)
+			{
+				free(list[i]->filename);
+				list[i]->filename = NULL;
+			}
+			i++;
+		}
+		free(list);
+		list = NULL;
+	}
 
 	void free_struct(t_msh *commands)
 	{
@@ -17,9 +32,9 @@
 			if (commands->cmds[count]->cmd_main)
 				free(commands->cmds[count]->cmd_main);
 			if (commands->cmds[count]->infile)
-				free(commands->cmds[count]->infile);
+				free_aux(commands->cmds[count]->infile);
 			if (commands->cmds[count]->outfile)
-				free(commands->cmds[count]->outfile);
+				free_aux(commands->cmds[count]->outfile);
 			free(commands->cmds[count]->cmd);
 			free(commands->cmds[count]);
 			count++;
@@ -29,4 +44,3 @@
 			free(commands->evar);
 		free(commands->input);
 	}
-
