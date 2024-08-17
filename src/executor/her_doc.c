4a576012510418a38d4ad50fa6e3dd7743fdb8fb
LUIS DAVID <luis.david.10@hotmail.com>
2024-08-17 13:26:09 +0200

    fix: generate name with counter to avoid colliding in case of two hecs of the same name

--- src/executor/her_doc.c
+++ src/executor/her_doc.c
@@ -6,7 +6,7 @@
 /*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 13:55:36 by ldiaz-ra          #+#    #+#             */
-/*   Updated: 2024/05/25 15:01:54 by ldiaz-ra         ###   ########.fr       */
+/*   Updated: 2024/08/17 13:24:59 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,53 @@ static int	write_tmp(int fd, char *name)
 	return (0);
 }
 
+void	check_malloc(char *count_char, char *name_tmp, char *tmp)
+{
+	if (!count_char || !name_tmp || !tmp)
+		_exit(2);
+}
+
+char *check_file(char *name)
+{
+	char	*tmp;
+	char	*count_char;
+	char	*name_tmp;
+	int		count;
+
+	count = 1;
+	count_char = ft_itoa(count);
+	name_tmp = ft_strjoin(name, count_char);
+	tmp = ft_strjoin("/tmp/.", name_tmp);
+	check_malloc(count_char, name_tmp, tmp);
+	while (access(tmp, F_OK) != -1)
+	{
+		free(count_char);
+		free(name_tmp);
+		free(tmp);
+		count++;
+		count_char = ft_itoa(count);
+		name_tmp = ft_strjoin(name, count_char);
+		tmp = ft_strjoin("/tmp/.", name_tmp);
+		check_malloc(count_char, name_tmp, tmp);
+	}
+	free(count_char);
+	free(name_tmp);
+	return tmp;
+}
+
 char	*her_doc(char *name)
 {
 	int		fd;
 	char	*tmpfile;
-
-	tmpfile = ft_strjoin(".", name);
-	if (!tmpfile)
-		return (NULL);
-	fd = open(tmpfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	
+	tmpfile = check_file(name);
+	fd = open(tmpfile, O_WRONLY | O_CREAT , 0777);
 	if (fd < 0)
 	{
 		free(tmpfile);
-		return (NULL);
+		_exit(2);
 	}
-	if (write_tmp(fd, name))
-	{
-		free(tmpfile);
-		close(fd);
-		return (NULL);
-	}
+	write_tmp(fd, name);
 	close(fd);
 	return (tmpfile);
 }
