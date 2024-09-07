2a5e52cf40b54c0e2015eaa74ebd23256f05ac44
LUIS DAVID <luis.david.10@hotmail.com>
2024-09-07 14:00:02 +0200

    fix: free infiles and outfile

--- src/free_struct.c
+++ src/free_struct.c
@@ -1,6 +1,6 @@
 	#include "../inc/minishell.h"
 
-	static void free_aux(t_msh *commands, int count)
+	static void free_infile(t_msh *commands, int count)
 	{
 		int i;
 
@@ -11,11 +11,33 @@
 			{
 				free(commands->cmds[count]->infile[i]->filename);
 				commands->cmds[count]->infile[i]->filename = NULL;
+				free(commands->cmds[count]->infile[i]);
+				commands->cmds[count]->infile[i] = NULL;
 			}
 			i++;
 		}
 		free(commands->cmds[count]->infile);
 		commands->cmds[count]->infile = NULL;
+	}
+
+	static void free_outfile(t_msh *commands, int count)
+	{
+		int i;
+
+		i = 0;
+		while (commands->cmds[count]->outfile[i])
+		{
+			if (commands->cmds[count]->outfile[i]->filename)
+			{
+				free(commands->cmds[count]->outfile[i]->filename);
+				commands->cmds[count]->outfile[i]->filename = NULL;
+				free(commands->cmds[count]->outfile[i]);
+				commands->cmds[count]->outfile[i] = NULL;
+			}
+			i++;
+		}
+		free(commands->cmds[count]->outfile);
+		commands->cmds[count]->outfile = NULL;
 	}
 
 	void free_struct(t_msh *commands)
@@ -32,9 +54,9 @@
 			if (commands->cmds[count]->cmd_main)
 				free(commands->cmds[count]->cmd_main);
 			if (commands->cmds[count]->infile)
-				free_aux(commands, count);
+				free_infile(commands, count);
 			if (commands->cmds[count]->outfile)
-				free_aux(commands, count);
+				free_outfile(commands, count);
 			free(commands->cmds[count]->cmd);
 			free(commands->cmds[count]);
 			count++;
