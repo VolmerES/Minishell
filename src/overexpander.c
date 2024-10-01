1df8fbb5155e25a3125b9f7bc05c1e1280a1a92f
Jdelorme <volmer394@gmail.com>
2024-10-01 13:28:34 +0200

    fix: Comillas se copian ahora correctamente en casos como "$SHLVL"

--- src/overexpander.c
+++ src/overexpander.c
@@ -36,7 +36,8 @@ char	*ft_get_var_name(char *input, int *i)
 
 	var_len = 1;
 	while (input[*i + var_len] != SPACE && input[*i + var_len] != '\0'
-		&& input[*i + var_len] != DOLLAR && input[*i + var_len] != DQUOTES)
+		&& input[*i + var_len] != DOLLAR && input[*i + var_len] != DQUOTES
+		&& input[*i + var_len] != SQUOTES)
 		var_len++;
 	return (ft_substr(input, *i + 1, var_len - 1));
 }
