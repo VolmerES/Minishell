9796e682e6163f89ad3d5df0a7e428c75de7fac5
Jdelorme <volmer394@gmail.com>
2024-10-19 15:01:53 +0200

    fix; liberaciones de memorias para quitar leaks

--- src/expander.c
+++ src/expander.c
@@ -6,7 +6,7 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 22:50:45 by jdelorme          #+#    #+#             */
-/*   Updated: 2024/10/15 15:38:40 by jdelorme         ###   ########.fr       */
+/*   Updated: 2024/10/19 14:58:15 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,8 @@ void	ft_expand(t_msh *commands)
 	int	i;
 
 	i = ft_search_env(commands->envp, commands->evar);
+	if (i ==  -1)
+		free(commands->evar);
 	commands->evar = ft_manage_expander(commands->envp, i, commands->evar);
 	if (commands->evar == NULL)
 		free(commands->evar);
