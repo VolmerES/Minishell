b34cac46b830ad7a6a3ffece695f78b3cd166442
LUIS DAVID <luis.david.10@hotmail.com>
2024-10-19 17:05:05 +0200

    fix: Fixing memory leak caused by Juan touching my code

--- src/builtins/export_manage.c
+++ src/builtins/export_manage.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_manage.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ldiaz-ra <ldiaz-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 12:08:39 by david             #+#    #+#             */
-/*   Updated: 2024/10/19 16:38:33 by jdelorme         ###   ########.fr       */
+/*   Updated: 2024/10/19 16:57:46 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,15 @@ void	manage_export(t_msh *commands, int num_command)
 			var_split = ft_split(commands->cmds[num_command]->args[i], '=');
 			if (!var_split)
 				exit(1);
-
 			if (ft_check_syntax(var_split[0], commands) == 0)
 			{
 				update_env(commands, ft_search_env(commands->envp, var_split[0]), \
 				ft_strdup(commands->cmds[num_command]->args[i]));
-				ft_free_matrix(var_split);
 				commands->last_out = 0;
 			}
+			else
+				commands->last_out = 1;
+			ft_free_matrix(var_split);
 		}
 		i++;
 	}
